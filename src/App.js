328222a5423f25427fab60accefed713185f1da7
Kurt Van Etten <kurt@vanetten.us>
2019-10-01 20:11:51 -0400

    Do some initial housekeeping

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const AppWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	margin-top: 30px;
+
+	h1 {
+		font-size: 5rem;
+	}
+`;
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+	constructor() {
+		super();
+
+	}
+
+
+
+
+	render() {
+		return (
+			<AppWrapper>
+				<div>
+					<h1>Todo!</h1>
+				</div>
+			</AppWrapper>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+import './reset.css';
+import './index.css';
+
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+
+render(
+		<App />,
+	document.getElementById('root'));
+
