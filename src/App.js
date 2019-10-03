c55318c9986570295f2b972663e7e6e85183d888
Kurt Van Etten <kurt@vanetten.us>
2019-10-02 21:27:15 -0400

    basic functionality completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 
@@ -9,30 +9,110 @@ const AppWrapper = styled.div`
 
 	margin-top: 30px;
 
+	font-size: 2rem;
+
 	h1 {
+		margin: 10px;
 		font-size: 5rem;
 	}
+
+	h3 {
+		margin: 5px;
+		font-size: 3rem;
+	}
+
+	.done {
+		text-decoration: line-through;
+	}
+
 `;
 
 
-class App extends React.Component {
-// you will need a place to store your state in this component.
-// design `App` to be the parent component of your application.
-// this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends Component {
 	constructor() {
 		super();
 
+		this.state = {
+			newtask: '',
+			todolist: []
+		};
 	}
 
 
+	submitHandler = (e) => {
+		e.preventDefault();
+
+		if (this.state.newtask === '') return;
+
+		const newTodo = {
+			id: Date.now(),
+			task: this.state.newtask,
+			completed: false
+		};
+
+		this.setState({
+			newtask: '',
+			todolist: [...this.state.todolist, newTodo]
+		});
+	}
+
+
+	resetForm = (e) => {
+		e.preventDefault();
+		const newlist = this.state.todolist.filter(item => !item.completed);
+
+		this.setState({...this.state, todolist: newlist});
+	}
+
+
+	toggleDone = (e, id) => {
+		e.preventDefault();
+
+		const newlist = this.state.todolist.map(item => ({
+			...item,
+			completed: item.id === id ? !item.completed : item.completed
+		}));
+
+		this.setState({...this.state, todolist: newlist});
+	}
+
+
+	handleChange = (e) => {
+		e.preventDefault();
+		this.setState({...this.state, [e.target.name]: e.target.value })
+	}
 
 
 	render() {
 		return (
 			<AppWrapper>
-				<div>
-					<h1>Todo!</h1>
-				</div>
+				<h1>To-Do!</h1>
+				<h3>Your Tasks:</h3>
+
+				<ul>
+					{this.state.todolist.map(todo => {
+						return (
+							<li key={todo.id}
+								className={todo.completed ? 'done' : ''}
+								onClick={e => this.toggleDone(e, todo.id)} >
+								{todo.task}
+							</li>
+						);
+					})}
+				</ul>
+				
+			    <form onSubmit={this.submitHandler}>
+				    <input
+				        onChange={this.handleChange}
+				        placeholder="Enter new task"
+				        type="text"
+				        name="newtask"
+				        value={this.state.newtask}
+				    />
+				    <button type="submit">Add Task</button>
+				    <button onClick={this.resetForm}>Clear Completed</button>
+			    </form>
 			</AppWrapper>
 		);
 	}
